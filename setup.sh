227dace842dfab154877f64e36b935c22127fde2
Atsushi Takayama <taka.atsushi@gmail.com>
2014-02-17 02:36:32 +0900

    modify terminfo setup for compatibility

--- setup.sh
+++ setup.sh
@@ -6,9 +6,13 @@ ln -s "$DIR/.gitconfig" ~/
 ln -s "$DIR/.gitexcludesfile" ~/
 ln -s "$DIR/.gitmodules" ~/
 ln -s "$DIR/.gitscreenrc" ~/
-ln -s "$DIR/.terminfo" ~/
 ln -s "$DIR/.vim" ~/
 ln -s "$DIR/.vimrc" ~/
 ln -s "$DIR/.zprofile" ~/
 ln -s "$DIR/.zshrc" ~/
 ln -s "$DIR/.zshrc.my" ~/
+
+cd "$DIR/.terminfo"
+tic mostlike.txt
+ln -s "$DIR/.terminfo" ~/
+cd -
