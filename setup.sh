4c6cf384ce053a475da9e345a0be7645b265d5cc
edvakf <edvakf@pixiv.com>
2014-02-17 01:52:18 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+DIR=`dirname $0`
+DIR=`cd $DIR; pwd`
+ln -s "$DIR/.gitconfig" ~/
+ln -s "$DIR/.gitexcludesfile" ~/
+ln -s "$DIR/.gitmodules" ~/
+ln -s "$DIR/.gitscreenrc" ~/
+ln -s "$DIR/.terminfo" ~/
+ln -s "$DIR/.vim" ~/
+ln -s "$DIR/.vimrc" ~/
+ln -s "$DIR/.zprofile" ~/
+ln -s "$DIR/.zshrc" ~/
+ln -s "$DIR/.zshrc.my" ~/
